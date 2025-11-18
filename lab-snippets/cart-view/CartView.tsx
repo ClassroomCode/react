94a6050d6364c488916ca32b3dd8d5f214a51150
Jason Bell <jason.bell@treeloop.com>
2025-11-17 19:16:24 -0800

    Lab snippets

---
+++ lab-snippets/cart-view/CartView.tsx
@@ -0,0 +1,32 @@
+import {LicensePlate} from '../license-plate/LicensePlate';
+import {Jumbotron} from '../jumbotron/Jumbotron';
+import {useCart} from "../cart-service/cart-hook";
+
+export function CartView(props: any) {
+
+	const [cartContents] = useCart();
+
+	let rows;
+	if (cartContents) {
+		rows = cartContents.map((licensePlate, index) => {
+			return (
+				<div key={licensePlate._id} className="col-md-4" style={{backgroundColor: (index % 2 === 0) ? '#F5F5F5' : ''}}>
+					<LicensePlate currency={props.currency} plate={licensePlate} buttonText="Remove from cart &times;" />
+				</div>
+			);
+		});
+	}
+	return (
+		<>
+			<Jumbotron title="My Cart" description="Your current cart contents:"/>
+			<div className="container">
+				{rows && rows.length === 0 && <div className="alert alert-info" role="alert">Your cart is empty</div>}
+				<div className="row" >
+					{rows}
+				</div>
+				<hr/>
+			</div>
+		</>
+	);
+}
+
---
+++ lab-snippets/license-plate-data.type.ts
@@ -0,0 +1,12 @@
+export interface LicensePlateData {
+    _id: string;
+    onSale: boolean;
+    picture: string;
+    title: string;
+    price: number;
+    year: number;
+    state: string;
+    description: string;
+}
+
+export type Currency = "USD" | "EUR" | "GBP";
